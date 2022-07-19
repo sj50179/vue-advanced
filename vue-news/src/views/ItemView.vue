f850edbf8df11271406b9397347496ff5b83d2a3
Brenden SJ Song <sj50179@gmail.com>
2022-07-19 15:23:36 +0900

    1st commit - router

---
+++ vue-news/src/views/ItemView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Item
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ vue-news/src/views/UserView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        User
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
